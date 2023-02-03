09a689021fe9bd951107af9df3dfd26092bc4c2d
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-02-03 11:20:06 -0500

    Update index.test.ts

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -4,10 +4,12 @@ import { IAMClient, ListAccountAliasesCommand } from "@aws-sdk/client-iam";
 import {
   SecurityHubClient,
   GetFindingsCommand,
+  AwsSecurityFinding,
 } from "@aws-sdk/client-securityhub";
 import { mockClient } from "aws-sdk-client-mock";
 import JiraClient from "jira-client";
 import sinon from "sinon";
+import { Jira } from "../libs";
 
 const searchJiraResponse = {
   issues: [
@@ -25,13 +27,27 @@ const searchJiraStub = sinon.stub(JiraClient.prototype, "searchJira");
 searchJiraStub.resolves(searchJiraResponse);
 
 const LIST_ACCOUNT_ALIASES_RESPONSE = {
-  $metadata: {
-    httpStatusCode: 200,
-  },
+  $metadata: {},
   AccountAliases: ["my-account-alias"],
 };
-const GET_FINDINGS_COMMAND_RESPONSE = {
-  Findings: [],
+const GET_FINDINGS_COMMAND_RESPONSE: {
+  Findings: AwsSecurityFinding[];
+  $metadata: {};
+} = {
+  Findings: [
+    {
+      SchemaVersion: undefined,
+      Id: undefined,
+      ProductArn: undefined,
+      GeneratorId: undefined,
+      AwsAccountId: undefined,
+      CreatedAt: undefined,
+      UpdatedAt: undefined,
+      Title: undefined,
+      Description: undefined,
+      Resources: undefined,
+    },
+  ],
   $metadata: {},
 };
 
@@ -50,8 +66,6 @@ beforeEach(() => {
 });
 
 describe("SecurityHubJiraSync", () => {
-  process.env.PROJECT = "myProject";
-
   describe("JiraClient Tests", async () => {
     it("happy path", async () => {
       const jira = new JiraClient({ host: "" });
@@ -67,6 +81,34 @@ describe("SecurityHubJiraSync", () => {
       const syncResult = await sync.sync();
 
       expect(syncResult).not.toBeDefined();
+    });
+  });
+
+  describe("Without JIRA_CLOSED_STATUSES environment variable", async () => {
+    delete process.env.JIRA_CLOSED_STATUSES;
+    // process.env.PROJECT = "test";
+    it("s path", async () => {
+      const jira = new JiraClient({ host: "" });
+      const jqlString =
+        'project = T4 AND labels = security-hub AND status not in ("Done")';
+      const result = await jira.searchJira(jqlString, {});
+      expect(result).toEqual(searchJiraResponse);
+    });
+  });
+
+  describe("missing a required environment variable", async () => {
+    //   expect(new SecurityHubJiraSync()).rejects.toThrow(
+    //     "Error: Missing required environment variables: PROJECT"
+    //   );
+
+    it("exception path", () => {
+      delete process.env.PROJECT;
+      const throwError = () => {
+        new Jira();
+      };
+      expect(throwError).toThrow(
+        "Missing required environment variables: PROJECT"
+      );
     });
   });
 });
