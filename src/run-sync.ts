119ff634d901d4045a3fb9651fc0b31f4235c1a6
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-02-13 15:31:57 -0500

    update example to match platform team's project

--- src/run-sync.ts
+++ src/run-sync.ts
@@ -3,4 +3,8 @@ import { SecurityHubJiraSync } from "./macpro-security-hub-sync";
 new SecurityHubJiraSync({
   region: "us-east-1",
   severities: ["CRITICAL", "HIGH"],
+  customJiraFields: {
+    customfield_14117: [{ value: "Dev Team" }],
+    customfield_14151: [{ value: "OneMac" }],
+  },
 }).sync();
