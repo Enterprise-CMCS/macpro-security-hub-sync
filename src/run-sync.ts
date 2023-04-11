12a35181c33db455d4f1ff3f95b4cebb92b98433
Jon Holman <9025884+JonHolman@users.noreply.github.com>
2023-04-11 16:38:29 -0400

    undo run-sync.ts change, unnecessary

--- src/run-sync.ts
+++ src/run-sync.ts
@@ -2,7 +2,7 @@ import { SecurityHubJiraSync } from "./macpro-security-hub-sync";
 
 new SecurityHubJiraSync({
   region: "us-east-1",
-  severities: ["CRITICAL", "HIGH", "MEDIUM"],
+  severities: ["CRITICAL", "HIGH"],
   customJiraFields: {
     customfield_14117: [{ value: "Dev Team" }],
     customfield_14151: [{ value: "OneMac" }],
